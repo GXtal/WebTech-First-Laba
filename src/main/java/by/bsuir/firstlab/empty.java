5b8d13df3223eb6bdca9973661681c8e6faf54c7
GXtal <zazaza00133@gmail.com>
2022-09-17 22:29:00 +0300

    just save after task 11

--- src/main/java/by/bsuir/firstlab/empty.java
+++ src/main/java/by/bsuir/firstlab/empty.java
@@ -14,6 +14,6 @@ public class empty {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        System.out.print("sssssss");
     }
 }
